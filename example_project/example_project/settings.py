ee329c3eec4c8b109d7cf04a9f76543af7f73d89
holgerd77 <Holger.Drewes@googlemail.com>
2014-08-18 10:51:48 +0200

    Changed language in example project settings to english

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -45,7 +45,7 @@ TIME_ZONE = 'Europe/Berlin'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
