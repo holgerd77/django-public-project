35e43325f74035fa71d919b175009b88748c82b3
holgerd77 <Holger.Drewes@googlemail.com>
2014-08-21 12:32:20 +0200

    Updated MANIFEST.in, setup.py in preparation for v.0.6 release, added example project files

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 import os
- 
+
 setup(
     name='django-public-project',
     version='0.6.0',
     description='CMS for building an information website around public projects',
     author='Holger Drewes',
-    author_email='Holger.Drewes@googlemail.com',
+    author_email='Holger.Drewes@gmail.com',
     url='https://github.com/holgerd77/django-public-project',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     packages=[
@@ -16,6 +16,12 @@ setup(
         'public_project.management.commands',
         'public_project.migrations',
         'public_project.templatetags',
+        'public_project.utils',
+        'example_project',
+        'example_project.ep_setup_app',
+        'example_project.ep_setup_app.management',
+        'example_project.ep_setup_app.management.commands',
+        'example_project.example_project',
     ],
     include_package_data=True,
     install_requires=[
