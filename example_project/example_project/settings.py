69938654070761976d311787c22bcf5c25adf9c6
holgerd77 <Holger.Drewes@googlemail.com>
2017-03-16 19:08:36 +0100

    Replaced example_project settings tuples with lists

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -12,9 +12,9 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # Adding root folder to sys path to have public_project app included
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "../.."))
 
-ADMINS = (
+ADMINS = [
     # ('Your Name', 'your_email@example.com'),
-)
+]
 
 MANAGERS = ADMINS
 
@@ -88,31 +88,31 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, '../static')
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+]
 
 # List of finder classes that know how to find static files in
 # various locations.
-STATICFILES_FINDERS = (
+STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+]
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'h4)3^hi%$fu^*(!7*#ttw9nhc)!s_qxje*3!&9u5ie(^yul$dn'
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
+TEMPLATE_LOADERS = [
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
-)
+]
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,19 +120,19 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'example_project.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'example_project.wsgi.application'
 
-TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + [
     "django.core.context_processors.request",
     "public_project.context_processors.uploaded_images_list",
-)
+]
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -143,7 +143,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'tastypie',
     'ep_setup_app', #only for example project, don't use in a real deployment
-)
+]
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
